93fdfece3eaff38518ab29bb446fac74cf5d83e4
mariacarl <fbrecio@up.edu.ph>
2018-12-10 16:21:24 +0800

    Added NavBar.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
+import NavBar from './NavBar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-    
+      
       </div>
     );
   }
